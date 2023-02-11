7d40e91d93d728cf0ed5a8313689aac0d8cfc147
Burak Bilgen <burakbilgendeveloper@gmail.com>
2023-02-11 17:25:55 +0300

    Add table view

---
+++ Gaming Center/Gaming Center/Features/TableView/GameListVC.swift
@@ -0,0 +1,27 @@
+//
+//  GameListVC.swift
+//  Gaming Center
+//
+//  Created by Burak Bilgen on 11.02.2023.
+//
+
+import UIKit
+
+class GameListVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
