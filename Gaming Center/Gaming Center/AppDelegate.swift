88bb69e24ea677d60a7b58787c06c7d781cc3f0b
Burak Bilgen <burakbilgendeveloper@gmail.com>
2023-02-11 15:47:23 +0300

    Setup project
    -Minimum deployment version changed to iOS 11.
    -Scene delegate has been removed (iOS 11 does not support the scene delegate feature).
    -Main storyboard file has been removed.

---
+++ Gaming Center/Gaming Center/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Gaming Center
+//
+//  Created by Burak Bilgen on 11.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
